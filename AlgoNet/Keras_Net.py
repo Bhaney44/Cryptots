76bcfefa431be2b9a403fbc0dbce1a2a8b2fd56d
B Haney <43055154+Bhaney44@users.noreply.github.com>
2021-04-03 14:05:14 -0400

    Add files via upload

---
+++ AlgoNet/Keras_Net.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+
+
+#Data
+#Add Dataset
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
